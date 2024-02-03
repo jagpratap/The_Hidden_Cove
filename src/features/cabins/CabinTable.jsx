f591d82474ba7bdc75b0740e58481d39eb6b9bae
jagpratap <jagpratapchauhan@gmail.com>
2024-02-03 15:06:54 +0530

    Integrate filter feature for cabins table

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,15 +1,32 @@
+import { useSearchParams } from "react-router-dom";
+
 import { useCabins } from "./useCabins";
 
 import Table from "../../ui/Table";
 import Spinner from "../../ui/Spinner";
+import Menus from "../../ui/Menus";
+import Empty from "../../ui/Empty";
 
 import CabinRow from "./CabinRow";
-import Menus from "../../ui/Menus";
 
 function CabinTable() {
+  const [searchParams] = useSearchParams();
   const { isLoading, cabins } = useCabins();
 
   if (isLoading) return <Spinner />;
+  if (!cabins.length) return <Empty resourceName="cabins" />;
+
+  // 1) FILTER
+  const filterValue = searchParams.get("discount") || "all";
+
+  let filteredCabins;
+  if (filterValue === "all") filteredCabins = cabins;
+  if (filterValue === "no-discount") {
+    filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
+  }
+  if (filterValue === "with-discount") {
+    filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+  }
 
   return (
     <Menus>
@@ -24,7 +41,7 @@ function CabinTable() {
         </Table.Header>
 
         <Table.Body
-          data={cabins}
+          data={filteredCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
---
+++ src/ui/Filter.jsx
@@ -0,0 +1,62 @@
+import styled, { css } from "styled-components";
+import { useSearchParams } from "react-router-dom";
+
+const StyledFilter = styled.div`
+  border: 1px solid var(--color-grey-100);
+  background-color: var(--color-grey-0);
+  box-shadow: var(--shadow-sm);
+  border-radius: var(--border-radius-sm);
+  padding: 0.4rem;
+  display: flex;
+  gap: 0.4rem;
+`;
+
+const FilterButton = styled.button`
+  background-color: var(--color-grey-0);
+  border: none;
+
+  ${(props) =>
+    props.active &&
+    css`
+      background-color: var(--color-brand-600);
+      color: var(--color-brand-50);
+    `}
+
+  border-radius: var(--border-radius-sm);
+  font-weight: 500;
+  font-size: 1.4rem;
+  padding: 0.44rem 0.8rem;
+  transition: all 0.3s;
+
+  &:hover:not(:disabled) {
+    background-color: var(--color-brand-600);
+    color: var(--color-brand-50);
+  }
+`;
+
+function Filter({ filterField, options }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentFilter = searchParams.get(filterField) || options[0].value;
+
+  function handleFilter(value) {
+    searchParams.set(filterField, value);
+    setSearchParams(searchParams);
+  }
+
+  return (
+    <StyledFilter>
+      {options.map(({ value, label }) => (
+        <FilterButton
+          key={value}
+          onClick={() => handleFilter(value)}
+          active={+(value === currentFilter)}
+          disabled={value === currentFilter}
+        >
+          {label}
+        </FilterButton>
+      ))}
+    </StyledFilter>
+  );
+}
+
+export default Filter;
