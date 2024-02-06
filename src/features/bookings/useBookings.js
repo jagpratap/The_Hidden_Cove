8fa1a7025f7025303338cd39a47416d1e32ab5b2
jagpratap <jagpratapchauhan@gmail.com>
2024-02-06 12:40:56 +0530

    Integrate api-side sorting feature for bookings table

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -7,12 +7,17 @@ export function useBookings() {
   const [searchParams] = useSearchParams();
 
   // 1. FILTER
-  const filterValue = searchParams.get("status");
-  const filter = !filterValue || filterValue === 'all' ? null : { field: 'status', value: filterValue };
+  const filterValue = searchParams.get("status") || 'all';
+  const filter = filterValue === 'all' ? null : { field: 'status', value: filterValue };
+
+  // 2. SORT
+  const sortValue = searchParams.get("sortBy") || 'start_date-desc';
+  const [field, direction] = sortValue.split("-");
+  const sortBy = { field, direction };
 
   const { isLoading, data: bookings, error } = useQuery({
-    queryKey: ['bookings', filter],
-    queryFn: () => getBookings({ filter })
+    queryKey: ['bookings', filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy })
   });
 
   return { isLoading, bookings, error };
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,13 +1,20 @@
 import supabase from "./supabase";
 
-export async function getBookings({ filter }) {
+export async function getBookings({ filter, sortBy }) {
   let query = supabase
     .from("bookings")
     .select("id, created_at, start_date, end_date, num_nights, num_guests, status, total_price, cabins(name), guests(full_name, email)");
 
-  // FILTER
+  // 1. FILTER
   if (filter) {
     query = query[filter.method || 'eq'](filter.field, filter.value);
+  }
+
+  // 2. SORT
+  if (sortBy) {
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === 'asc',
+    });
   }
 
   const { data, error } = await query;
