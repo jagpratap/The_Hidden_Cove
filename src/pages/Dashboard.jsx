4322e2810f72741adc433639da1752d8579c9dfe
jagpratap <jagpratapchauhan@gmail.com>
2024-02-15 23:18:56 +0530

    Create dashboard layout

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,20 @@
+import Row from "../ui/Row";
+import Heading from "../ui/Heading";
+
+import DashboardFilter from "../features/dashboard/DashboardFilter";
+import DashboardLayout from "../features/dashboard/DashboardLayout";
+
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">Dashboard</Heading>
+        <DashboardFilter />
+      </Row>
+
+      <DashboardLayout />
+    </>
+  );
 }
 
 export default Dashboard;
